bafc0722de513251c714cbc523fa903dda76cc45
unknown <Mariafernandasantillanavila>
2023-05-19 00:09:31 -0600

    SE AGREGA HOLA MUNDO

---
+++ 1 HolaMundo/src/pkg1/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg1.holamundo;
+
+
+public class HolaMundo {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println ("HOLA MUNDO : ");
+    }
+    
+}
